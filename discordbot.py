497f08de1cb5e136156bf46c6c261423d7f963ab
Czar <czarolag@gmail.com>
2023-04-24 00:19:52 +0000

    Added functional example commands for bot

--- discordbot.py
+++ discordbot.py
@@ -1,32 +1,40 @@
 import lightbulb
 from hikari import Embed
 import gspread
+import json
 
 import google.auth
 from google.auth.transport.requests import Request
 
+'''
+Method returns needed data for the bot to function, using key, value pairs from 'bot_data.json'
+'''
+def getData(key: str):
+    with open('bot_data.json', 'r') as f:
+        data = json.load(f)
+
+    value = data[key]
+    return value
+
+
 # Authenticate the bot by creating it and adding the token
 # More on this: https://discord.com/developers/docs/intro
 bot = lightbulb.BotApp(
-    token='Your Bot Token Here',
+    token=getData("token"),
     default_enabled_guilds=(
-        0000000000000000, # Your discord Server ID here. Can be more than one server (Servers are also known as Guilds)
-        1111111111111111,
+        getData("guild_1")
         )
 )
 
-SPREADSHEET_ID = 'Google Spreadsheet ID here'
-creds = None
-
 # credentials.json for service account must be in same directory. Steps can be found: https://mljar.com/blog/authenticate-python-google-sheets-service-account-json-credentials/
-creds, project_id = google.auth.load_credentials_from_file('Your_Service_Account_Credentials.json', scopes=['https://www.googleapis.com/auth/spreadsheets'])
+creds, project_id = google.auth.load_credentials_from_file('credentials.json', scopes=['https://www.googleapis.com/auth/spreadsheets'])
 
 if not creds or not creds.valid:
     creds.refresh(google.auth.transport.requests.Request())
 
 
 '''
-Command Options can be used within discord chat
+Example Command used to display commands created.
 '''
 @bot.command
 @lightbulb.command('bot-help', 'Provides commands') # command to be used from discord ex: /bot-help ~ provides commands
@@ -34,7 +42,8 @@ Command Options can be used within discord chat
 async def help(ctx):
     # example list to display various commands. Can append more commands if needed
     info = [
-            '/bot-add {information}',
+            '/bot-display {column}',
+            '/bot-temp {example for own use}'
             ]
 
     # creates an embed in discord. More about embeds: https://b1naryth1ef.github.io/disco/bot_tutorial/message_embeds.html
@@ -49,29 +58,33 @@ async def help(ctx):
 
 
 '''
-Command Option with user input
+Example Command displays a column from the Google Sheets to the discord server
 '''
 @bot.command
-@lightbulb.option('info_one', 'Input information 1', type=int)
-@lightbulb.option('info_two', 'Input information 2', type=str)
-@lightbulb.command("bot-addInfo", 'adds information to Google Spreadsheet')
-@lightbulb.implements(lightbulb.SlashCommand) # Command: /bot-addInfo info_one info_two
-async def equipment_max(ctx):
+@lightbulb.option('column', 'Column to display', type=int)
+@lightbulb.command('bot-display', 'Displays a column on the google sheets')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def display(ctx):
+    # Uses credentials.json to recieve service account
+    gc = gspread.service_account(filename='credentials.json')    
 
-    # activates service account with given credentials.json
-    gc = gspread.service_account(filename='Your_Service_Account_Credentials.json')
+    # Used to modify information of Google Sheets using ID recieved from 'bot_data.json'
+    # More about gspread API commands: https://github.com/burnash/gspread/tree/v5.7.1
+    sheet_id = getData("sheet_id_1")
+    wks = gc.open_by_key(sheet_id)
+    sh = wks.get_worksheet(0)
 
-    # Used to modify information on Google Sheets using the Sheets ID
-    # More about gsspread API commands: https://github.com/burnash/gspread/tree/v5.7.1
-    sheet = gc.open_by_key(SPREADSHEET_ID)
-    worksheet = sheet.worksheet('example_sheet')
-    worksheet.update('A1', ctx.options.info_one) # ctx.options.example recieves the inputted information 
-    worksheet.update('B1', ctx.options.info_two) # from the decorator @lightbulb.option('example', 'example info')
+    # get info in a column
+    values_list = sh.col_values(ctx.options.column)
 
-    # using ctx.author.id in this format returns the username of the user who
-    # has sent the command request
-    await ctx.respond(f'<@{ctx.author.id}> your info has been updated')
+    embed = Embed(
+            title=f"Info From Column {ctx.options.column}",
+            description='\n'.join(values_list),
+            color=0x2b76c3,
+        )
 
+    # respond the information onto the server
+    await ctx.respond(embed=embed)
 
 # runs the bot
 bot.run()
